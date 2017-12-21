3d8505ac0e5bafd57a5c8912b6205b49d7c3a589
lilypadslulling <34723619+lilypadslulling@users.noreply.github.com>
2017-12-21 18:57:46 -0500

    Main Game Update
    Added those shiny buttons and backgrounds lol :D
    Left 2 Do: Clock, Cashier, Score, Misc Buttons

---
+++ WinterBreakGame/Assets/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Color A;
+    public Color B;
+    public float timer;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        spriteRenderer.color = Color.Lerp(A, B,Time.time * timer * Time.deltaTime);
+    }
+}
