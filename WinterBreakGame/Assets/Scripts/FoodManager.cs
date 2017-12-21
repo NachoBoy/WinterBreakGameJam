594b30c3b9d57bc2b8fd2ce4ee48662f02feefc4
NachoBoy <33005541+NachoBoy@users.noreply.github.com>
2017-12-20 19:16:43 -0500

    #PushForLark2017

---
+++ WinterBreakGame/Assets/Scripts/FoodManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour {
+
+    [HideInInspector]
+    public enum Food
+    {
+        Water,
+        Soda,
+        Milk,
+        Juice,
+        Hamburger,
+        Cheeseburger,
+        ChickenSandwhich,
+        VeganSandwhich,
+        Fries,
+        Chili,
+        Apples,
+        Rice
+    }
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ WinterBreakGame/Assets/Scripts/Order.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour {
+
+    [HideInInspector]
+    public List<FoodManager.Food> foodItems;
+
+    [HideInInspector]
+    public int numOfItems = Random.Range(0, 3);
+
+    // Use this for initialization
+    void Start()
+    {
+        for(int j = 0; j < numOfItems; j++)
+        {
+            foodItems[j] = (FoodManager.Food)Random.Range(0, System.Enum.GetValues(typeof(FoodManager.Food)).Length);
+        }
+
+        print("I would like a " + foodItems[0]);
+        for(int i = 1; i < numOfItems; i++)
+        {
+            print("");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
