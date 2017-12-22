91f33edcc7d7239a4aa0e47918dfaf519b993e8e
NachoBoy <33005541+NachoBoy@users.noreply.github.com>
2017-12-21 22:16:14 -0500

    Score Tracking  and end game scene link

---
+++ WinterBreakGame/Assets/Scripts/FinalScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FinalScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = ScoreManager.score.ToString();
+    }
+	
+}
