d19c35300a5132f223f000599edeeee65e88ed9c
bishies <33171456+bishies@users.noreply.github.com>
2017-12-21 23:20:24 -0500

    Main and End screen

---
+++ WinterBreakGame/Assets/EndNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndNavigation : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void ReturntoMain()
+    {
+        Debug.Log("Main");
+        SceneManager.LoadScene(0);
+    }
+}
