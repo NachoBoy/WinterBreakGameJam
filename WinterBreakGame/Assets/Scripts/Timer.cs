7ec8b8003fc1a476710fd2ad1291888f476631c0
lilypadslulling <34723619+lilypadslulling@users.noreply.github.com>
2017-12-21 20:26:59 -0500

    Timer LEL
    UUUUUUUUUUUUUUUUUUUUUUH

---
+++ WinterBreakGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 180f; //time for level 
+    Text text;
+    private int timeLeftInt;
+
+	// Use this for initialization
+	void Awake() {
+        text = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timeLeftInt = (int)timeLeft;
+        text.text = (timeLeftInt.ToString());
+        if(timeLeft < 0)
+        {
+            SceneManager.LoadScene("LarkTestScene");
+        }
+	}
+}
