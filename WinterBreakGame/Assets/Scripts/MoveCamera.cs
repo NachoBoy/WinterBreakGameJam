b971e0aae449e037a986299a34fed16d9f95dc2f
lilypadslulling <34723619+lilypadslulling@users.noreply.github.com>
2017-12-21 22:12:41 -0500

    LERP LERP
    hewwo

--- WinterBreakGame/Assets/Scripts/MoveCamera.cs
+++ WinterBreakGame/Assets/Scripts/MoveCamera.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class MoveCamera : MonoBehaviour {
 
     //variables
-    public float movementTime = 2.0f; //amount of time the camera moves
+    public float movementTime = 0.4f; //amount of time the camera moves
     public float movementDistance = 9.2f; //distance the camera moves
     public bool register = false; //if the player is currently looking at the register screen or not
     public bool inMotion = false; //if the screen is currently moving - prevent mouse clicks from firing anything during the process
@@ -47,7 +47,7 @@ public class MoveCamera : MonoBehaviour {
 
         while (currentTime <= movementTime)
         {
-            float movementFactor = currentTime / movementTime;
+            float movementFactor = (Mathf.Sin(Mathf.PI * currentTime / movementTime - Mathf.PI/2) + 1)/2;
             this.transform.position = Vector3.Lerp(currentPosition, targetPosition, movementFactor);
             currentTime += Time.deltaTime;
             yield return null;
@@ -64,7 +64,7 @@ public class MoveCamera : MonoBehaviour {
 
         while (currentTime <= movementTime)
         {
-            float movementFactor = currentTime / movementTime;
+            float movementFactor = (Mathf.Sin(Mathf.PI * currentTime / movementTime - Mathf.PI / 2) + 1) / 2;
             this.transform.position = Vector3.Lerp(currentPosition, targetPosition, movementFactor);
             currentTime += Time.deltaTime;
             yield return null;
